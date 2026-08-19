b7f7f7130b47fc433b8bf3175d19e7299a0646aa
은태현 <unimokw@naver.com>
2026-08-19 16:00:48 +0900

    style: Prettier 포맷 일괄 적용

--- src/components/practices/basic/EventHandlerBasic.vue
+++ src/components/practices/basic/EventHandlerBasic.vue
@@ -8,13 +8,13 @@ const showAlert = () => {
 </script>
 
 <template>
-    <div class="practice-section">
-        <h2>v-on 이벤트 핸들러</h2>
-        <h3>1) 인라인 연산 처리</h3>
-        <p>현재 카운트: {{ count }}</p>
-        <button @click="count++">1 증가</button>
-        <br />
-        <h3>2) 스크립트 함수 호출</h3>
-        <button @click="showAlert">알림창 띄우기</button>
-    </div>
+  <div class="practice-section">
+    <h2>v-on 이벤트 핸들러</h2>
+    <h3>1) 인라인 연산 처리</h3>
+    <p>현재 카운트: {{ count }}</p>
+    <button @click="count++">1 증가</button>
+    <br />
+    <h3>2) 스크립트 함수 호출</h3>
+    <button @click="showAlert">알림창 띄우기</button>
+  </div>
 </template>
--- src/components/practices/basic/VueHtmlXss.vue
+++ src/components/practices/basic/VueHtmlXss.vue
@@ -10,10 +10,10 @@ function showMessage() {
 </script>
 
 <template>
-    <div class="practice-section">
-        <h2>v-html XSS 학습</h2>
-        <input v-model="inputValue" placeholder="메시지를 입력하세요" />
-        <button @click="showMessage">표시</button>
-        <div v-html="message"></div>
-    </div>
+  <div class="practice-section">
+    <h2>v-html XSS 학습</h2>
+    <input v-model="inputValue" placeholder="메시지를 입력하세요" />
+    <button @click="showMessage">표시</button>
+    <div v-html="message"></div>
+  </div>
 </template>
